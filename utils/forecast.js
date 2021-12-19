f8debd927c5ed30e5e68713094ef27c752578dd3
Shivika Tyagi <iamshivikatyagi@gmail.com>
2021-12-19 22:20:16 +0530

    some updation

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,13 +2,13 @@ const request = require("request")
 
 const forecast=(latitude, longitude, callback) =>{
     const url = 'http://api.weatherstack.com/current?access_key=3534a4f888605991fb027eab2dad71c7&query=' + latitude + ',' + longitude + '&units=f'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('unable to connect to  weather service', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('unable to find location', undefined)
         } else {
-            callback(undefined, 'It is currently ' + response.body.current.temperature + ' .F out' + ' and there is ' + response.body.current.precip + '% chance of rain.')
+            callback(undefined, 'It is currently ' + body.current.temperature + ' .F out' + ' and there is ' + body.current.precip + '% chance of rain.')
         }
     })
 }
