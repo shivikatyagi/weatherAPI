f0379cd0cc75a443d261cf780149a146f80b35fb
Shivika Tyagi <iamshivikatyagi@gmail.com>
2021-12-19 20:53:52 +0530

    addressing

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const request= require('request')
-const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-// const url = 'http://api.weatherstack.com/current?access_key=3534a4f888605991fb027eab2dad71c7&query=37.8267,-122.4233'
+const geocode = require('./utils/geocode')
 
+// const url = 'http://api.weatherstack.com/current?access_key=3534a4f888605991fb027eab2dad71c7&query=37.8267,-122.4233'
 // request({url: url, json:true},(error,response) =>{
 // // console.log(response.body.current)
 // console.log("it is currently "+ response.body.current.temperature + " degrees out but it feeels like " + response.body.current.feelslike + " degrees out")
 // })
 
-
-
-
 //const geocodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaWFtc2hpdmlrYXR5YWdpIiwiYSI6ImNreGJrOHZvMDA4emcybm1teXRyOGFlY3gifQ.KYUFqdq68RORFq9ovRqlIw'
-
 //  request({url: geocodeurl,json:true},(error,response) =>{
 //     const longitude = response.body.features[0].centre[0]
 //     const latitude = response.body.features[0].centre[1]
@@ -31,17 +27,28 @@ const forecast = require('./utils/forecast')
 //     }
 // })
 
+const address = process.argv[2]
 
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
 
-forecast(-75.7088,44.1545,(error,data) =>{
-    console.log('Error',error)
-    console.log('Data',data)
-})
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
+console.log(process.argv)
 
 
 
